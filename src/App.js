2c74be0ec2b523631f8e985a83d968f2ae7a29d8
johirjohnny <johirjohnny96@gmail.com>
2021-03-09 19:43:33 +0600

    DOne to see next

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'
+import Friend from './Component/Friend/Friend';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Component/Home/Home';
+import Nomatch from './Component/Nomatch/Nomatch';
+import FriendDetail from './Component/FriendDetail/FriendDetail';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+       <Switch>
+         <Route path = "/home">
+            <Home/>
+         </Route>
+         <Route path = "/friend/:friendId">
+            <FriendDetail/>
+         </Route>
+         <Route exact path ="/">
+           <Home/>
+         </Route>
+         <Route path ="*">
+           <Nomatch/>
+         </Route>
+       </Switch>
+    </Router>
   );
 }
 
